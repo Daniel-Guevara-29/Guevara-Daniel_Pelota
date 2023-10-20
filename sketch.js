274497b018f16c47844a1a83719ebb1c86a6aabe
daniel <daniel.guevara@ucuenca.edu.ec>
2023-10-20 12:15:49 -0500

    corrección final pelota

--- sketch.js
+++ sketch.js
@@ -8,20 +8,7 @@ let isBlackBackground = true;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // print(posX);
-  // posX = 234;
-  // print(posX);
-  // posX = posX + 25;
-  // print(posX);
-  // posX = posX * 2;
-  // print(posX);
-  // posX = posX / 5;
-  // print(posX);
-  // posX *= 8;
-  // print(posX);
-  // posX += 1;
-  // print(posX);
-  // posX ++;
+
 
   posX = windowWidth / 2;
   posY = windowHeight / 2; 
